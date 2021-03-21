2e215ff799baf08ae08e72a1978a4742579752d2
TheCandyQueen <73914684+TheCandyQueen@users.noreply.github.com>
2021-03-21 19:49:43 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -26,6 +26,10 @@ function add() {
     car1_imgTag = new Image();
     car1_imgTag.onload = uploadcar1;
     car1_imgTag.src = car1_image;
+
+    car2_imgTag = new Image();
+    car2_imgTag.onload = uploadcar2;
+    car2_imgTag.src = car2_image;
 }
 
 function uploadBackground() {
@@ -73,4 +77,92 @@ function my_keydown(e) {
             car2_right();
             console.log("d key")
         }
+}
+
+function car1_up() {
+    if(car1_y >=0) {
+        car1_y = car1_y - 10;
+        console.log("when up pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_down() {
+    if(car1_y <=500) {
+        car1_y = car1_y + 10;
+        console.log("when down pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_left() {
+    if(car1_y >=0) {
+        car1_x = car1_x - 10;
+        console.log("when left pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_right() {
+    if(car1_x <=700) {
+        car1_x = car1_x + 10;
+        console.log("when right pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+
+//CAR 2 CONTROLZZZZZZZZZZZ
+
+function car2_up() {
+    if(car1_y >=0) {
+        car1_y = car1_y - 10;
+        console.log("when up pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_down() {
+    if(car1_y <=500) {
+        car1_y = car1_y + 10;
+        console.log("when down pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_left() {
+    if(car1_y >=0) {
+        car1_x = car1_x - 10;
+        console.log("when left pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_right() {
+    if(car1_x <=700) {
+        car1_x = car1_x + 10;
+        console.log("when right pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+if(car1_x > 700) {
+    console.log("ca1 DA LUCKYYYYYY WINNAHHHHHh");
+    document.getElementById('game_status').innerHTML = "Car 1 da LUCKYYYYYYYYYYYY WINNAHHHHHHHHHHH <br> car 2 u poo";
 }
